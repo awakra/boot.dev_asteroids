22da80978092010fd25f93ff5c1342b36f583fcb
awakra <lucasadls0@gmail.com>
2025-01-24 18:24:27 -0300

    Game loop started

---
+++ main.py
@@ -0,0 +1,27 @@
+import pygame
+from constants import *
+
+def main():
+    print("Starting asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+    print(f"Screen asteroid min radius: {ASTEROID_MIN_RADIUS}")
+    print(f"Screen asteroid kinds: {ASTEROID_KINDS}")
+    print(f"Screen asteroid spawn rate: {ASTEROID_SPAWN_RATE}")
+    print(f"Screen asteroid max radius: {ASTEROID_MAX_RADIUS}")  
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill(0)
+        pygame.display.flip()
+        
+    
+
+if __name__ == "__main__":
+    main()
+    
+    
+    
